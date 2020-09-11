2b44dd41157afadec13ecc7caee50c638c675fa5
Y-lonelY <luckyyoung666@gmail.com>
2020-09-11 10:33:59 +0800

    feat: support typescript

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,17 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 
+const inputPath = {
+  js: 'src/index.js',
+  ts: 'src/typescript/index.tsx'
+}
+
 export default {
-  input: 'src/index.js',
+  input: inputPath.ts,
   output: {
     file: 'index.js',
     format: 'cjs',
+    exports: "auto"
   },
   plugins: [typescript(), resolve(), commonjs()],
   external: ["react", "react-dom"]
