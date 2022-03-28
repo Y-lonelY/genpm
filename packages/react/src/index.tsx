d2327c5e0693170342977bc10942758ca97a7255
Y-lonelY <luckyyoung666@gmail.com>
2022-03-28 22:27:04 +0800

    feat: lint react

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { WrapperProps } from './index.d'
 
 const Wrapper: React.FC<WrapperProps> = (props) => {
-  const { name } = props
+  const { name, children } = props
   return (
     <>
       <div>hi, {name}</div>
-      {props.children}
+      {children}
     </>
   )
 }
