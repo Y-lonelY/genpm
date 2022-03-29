1d4942dd5b0877e418380728d2000fdd9292fc25
Y-lonelY <luckyyoung666@gmail.com>
2022-03-29 22:09:18 +0800

    feat: ts control script

---
+++ packages/ts/scripts/control.mjs
@@ -0,0 +1,50 @@
+#!/usr/bin/env zx
+
+import chalk from 'chalk'
+
+const log = console.log
+const ALERT_MESSAGE = '\nPlease confirm your input!\n'
+const cmds = ['build']
+const [...restData] = process.argv.slice(3)
+
+let choosed
+if (!restData || restData.length === 0) {
+  const crt = await question('Choose command: ', {
+    choices: cmds,
+  })
+  choosed = crt.split(' ')
+} else {
+  choosed = restData
+}
+console.log(choosed)
+const [target, ...rest] = choosed
+switch (target) {
+  case 'build':
+    process.env.NODE_ENV = 'production'
+    build(rest)
+    break
+  default:
+    log(chalk.red(ALERT_MESSAGE))
+    log(`Support command ===> ${cmds.join(' ')}`)
+}
+
+function preHandleRest(data) {
+  const crt = {}
+  if (Array.isArray(data) && data.length > 0) {
+    data.forEach((item) => {
+      if (item.includes('=')) {
+        const [key, value] = item.split('=')
+        crt[key] = value
+      }
+    })
+  }
+  return crt
+}
+
+async function build(values) {
+  const params = preHandleRest(values)
+  process.env.ROLLUP_MODE = params.mode || false
+
+  await $`rollup -c`
+  log(chalk.white.bgGreen.bold(`Successfully built at ${Date.now()}`))
+}
